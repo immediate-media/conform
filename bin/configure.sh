4434fada3d461dac71b738282992fc29ab8a6c1a
alexandermendes <alexanderhmendes@gmail.com>
2020-09-18 12:02:37 +0100

    fix: use https for git urls

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,7 +1,6 @@
 configure_git() {
-  # Because some of our transitive JS depdencies reference github URLs
-  # directly, which creates issues with SSH.
-  git config --global url."https://github.com/".insteadOf git@github.com:
+  # Because some of our transitive JS depdencies reference git URLs directly,
+  # which creates issues with SSH.
   git config --global url."https://".insteadOf git://
 
   print_success "Using HTTPS for git URLs"
